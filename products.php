ed21a023b9a425772f5d467638676a74c4964e6c
trinity546 <katymcintosh@katys-mbp.wifi.bcit.ca>
2016-02-25 14:32:42 -0800

    fixed?

--- products.php
+++ products.php
@@ -15,23 +15,9 @@ loadScripts();
             $sku = $row['SKU'];
             $price = $row['item_price'];
             $desc = $row['description'];
-<<<<<<< Updated upstream
             $img = $row['path'];
             $name = $row['name'];
-            $html .= "<tr>
-                        <td data-sku-desc='$sku'>$desc</td>
-                        <td data-sku-desc='$sku'>$name</td>
-=======
-            $name = $row['name'];
-            $html .= "<tr>
-                        <td data-sku-desc='$sku'>$desc</td>
-                        <td data-sku-name='$sku'>$name</td>
->>>>>>> Stashed changes
-                        <td><input data-sku-qty='$sku' type='number' value='1' min='1' max='10' step='1'/></td>
-                        <td data-sku-price='$sku'>$price</td>
-                        <td><input data-sku-add='$sku' type='button' class='startCart' value='Add'/></td>
-                        <td><img src='$img'> </td>
-                      </tr>";
+            $html .= "";
         }
         echo $html;
         return;
